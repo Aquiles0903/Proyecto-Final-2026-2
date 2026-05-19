1595ce17539abd280fed9cb4c86f049d0d90f782
Aquiles0903 <dilver.2306@gmail.com>
2026-05-19 10:19:46 -0600

    Subiendo proyecto...

---
+++ GestionEmpleados/src/Empleado.java
@@ -0,0 +1,17 @@
+public class Empleado {
+    private int identificador;
+    private String nombre;
+    private String puesto;
+    private double salario;
+
+    public Empleado(int identificador, String nombre, String puesto, double salario){
+
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+
+    }
+
+    
+}
